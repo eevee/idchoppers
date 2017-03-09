0b0d66c31c199e492bd7994de4bff02137792e7f
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2017-03-09 15:40:58 -0800

    Identify maps, parse them a bit, dump to SVG

--- src/bin/idchoppers.rs
+++ src/bin/idchoppers.rs
@@ -1,17 +1,59 @@
-extern crate idchoppers;
-
 use std::io::{self, Read};
 
-use idchoppers::parse_wad;
+extern crate idchoppers;
+extern crate nom;
+use nom::IResult;
+extern crate svg;
+use svg::Document;
+use svg::node::Node;
+use svg::node::element::Line;
 
 fn main() {
     let mut buf = Vec::new();
     io::stdin().read_to_end(&mut buf);
 
-    if let Some(wad) = parse_wad(buf.as_slice()) {
+    if let IResult::Done(_leftovers, wad) = idchoppers::parse_wad(buf.as_slice()) {
         println!("found {:?}, {:?}, {:?}", wad.header.identification, wad.header.numlumps, wad.header.infotableofs);
-        for entry in wad.directory.iter() {
-            println!("{:8x}  {:8}  {}", entry.filepos, entry.size, entry.name);
+        for map_range in wad.iter_maps() {
+            if let Some(bare_map) = idchoppers::parse_doom_map(&wad, &map_range) {
+                write_bare_map_as_svg(&bare_map);
+                break;
+            }
         }
     }
+}
+
+fn write_bare_map_as_svg(map: &idchoppers::BareDoomMap) {
+    let mut doc = Document::new();
+    let mut minx = 0;
+    let mut miny = 0;
+    let mut maxx = 0;
+    let mut maxy = 0;
+    for vertex in map.vertices.iter() {
+        if vertex.x < minx {
+            minx = vertex.x;
+        }
+        if vertex.x > maxx {
+            maxx = vertex.x;
+        }
+        if vertex.y < miny {
+            miny = vertex.y;
+        }
+        if vertex.y > maxy {
+            maxy = vertex.y;
+        }
+    }
+
+    for line in map.lines.iter() {
+        let v0 = &map.vertices[line.v0 as usize];
+        let v1 = &map.vertices[line.v1 as usize];
+        let classname;
+        if line.front_sidedef == -1 || line.back_sidedef == -1 {
+            classname = "onesided";
+        }
+
+        doc.append(Line::new().set("x1", v0.x).set("y1", v0.y).set("x2", v1.x).set("y2", v1.y).set("stroke-width", 1).set("stroke", "black"));
+    }
+    doc.assign("viewBox", (minx, miny, maxx - minx, maxy - miny));
+    svg::save("idchoppers-temp.svg", &doc).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,86 @@
+extern crate core;
+use core::str::FromStr;
+use std::str;
+use std::u8;
+
 #[macro_use]
 extern crate nom;
+use nom::{IResult, Needed, digit, le_i16, le_u32};
 
-use std::str;
 
-use nom::{IResult, Needed, le_u32};
+
+// TODO Doom 64?  PlayStation Doom?  others?
+pub enum SourcePort {
+    // TODO distinguish doom 1 and 2, since they have different sets of things?  final doom?
+    Doom,
+    Heretic,
+    Hexen,
+    Strife,
+    Boom,
+    // TODO boom plus skybox transfer
+    // TODO try to distinguish versions of these...??
+    ZDoom,
+    GZDoom,
+
+    // I don't know so much about these
+    MBF,
+    Legacy,
+    Eternity,
+    Vavoom,
+}
+
+pub enum BaseGame {
+    None,  // i.e. this IS a base game, or is a TC, or whatever
+    Doom1,  // TODO distinguish from ultimate doom?
+    Doom2,
+    TNTEvilution,
+    Plutonia,
+    Heretic,
+    Hexen,
+    Strife,
+}
+
+pub enum MapName {
+    ExMy(u8, u8),
+    MAPxx(u8),
+}
+
+pub enum MapFormat {
+    Doom,
+    Hexen,
+    UDMF,
+}
+
+
+
+named!(exmy_map_name(&[u8]) -> MapName, do_parse!(
+    tag!(b"E") >>
+    e: digit >>
+    tag!(b"M") >>
+    m: digit >>
+    eof!() >>
+    (MapName::ExMy(
+        // We already know e and m are digits, so this is all safe
+        u8::from_str(str::from_utf8(e).unwrap()).unwrap(),
+        u8::from_str(str::from_utf8(m).unwrap()).unwrap(),
+    ))
+));
+
+named!(mapxx_map_name(&[u8]) -> MapName, do_parse!(
+    tag!(b"MAP") >>
+    x1: digit >>
+    x2: digit >>
+    eof!() >>
+    (MapName::MAPxx(
+        // TODO i can't help but feel that this is suboptimal
+        10 * u8::from_str(str::from_utf8(x1).unwrap()).unwrap()
+        + u8::from_str(str::from_utf8(x2).unwrap()).unwrap()
+    ))
+));
+
+named!(vanilla_map_name(&[u8]) -> MapName, alt!(exmy_map_name | mapxx_map_name));
+
+
 
 #[derive(Debug)]
 pub enum WADType {
@@ -58,55 +135,363 @@ fn wad_directory<'a>(buf: &'a [u8], header: &WADHeader) -> IResult<&'a [u8], Vec
     let mut ret = Vec::with_capacity(lumpct);
     let mut parse_from = &buf[offset..];
     for _ in 0..lumpct {
-        match wad_entry(parse_from) {
-            IResult::Done(leftovers, entry) => {
-                parse_from = leftovers;
-                ret.push(entry);
-            }
-            // TODO this seems unnecessarily verbose
-            IResult::Incomplete(needed) => {
-                return IResult::Incomplete(needed);
-            }
-            IResult::Error(err) => {
-                return IResult::Error(err);
-            }
-        }
+        let (leftovers, entry) = try_parse!(parse_from, wad_entry);
+        ret.push(entry);
+        parse_from = leftovers;
     }
     return IResult::Done(parse_from, ret);
 }
 
 
+// TODO:
+// - get entry by name (REMEMBER: NAMES CAN REPEAT)
+// - detect entry type (using name as a hint as well)
+// - how the hell do i iterate over /either/ an entry or a map?  maybe that's not even a useful thing to do
 pub struct WADArchive<'a> {
     pub buffer: &'a [u8],
     pub header: WADHeader,
     pub directory: Vec<WADDirectoryEntry<'a>>,
 }
 
+pub struct WADEntry<'a> {
+    pub archive: &'a WADArchive<'a>,
+    pub index: usize,
+}
+
+impl<'a> WADArchive<'a> {
+    pub fn iter_maps(&'a self) -> WADMapIterator<'a> {
+        return WADMapIterator{ archive: self, entry_iter: self.directory.iter().enumerate() };
+    }
+
+    pub fn entry_slice(&'a self, index: usize) -> &'a [u8] {
+        let entry = &self.directory[index];
+        let start = entry.filepos as usize;
+        let end = start + entry.size as usize;
+        return &self.buffer[start..end];
+    }
+}
+
+// TODO problems to scan a wad map for:
+// - missing a required lump
+// TODO curiosities to scan a wad map for:
+// - content in ENDMAP
+// - UDMF along with the old-style text maps
+
 
 // FIXME use Result, but, figure out how to get an actual error out of here
 // FIXME actually this fairly simple format is a good place to start thinking about how to return errors in general; like, do i want custom errors for tags?  etc
-pub fn parse_wad(buf: &[u8]) -> Option<WADArchive> {
-    match wad_header(buf) {
-        IResult::Done(_leftovers, header) => {
-            if let IResult::Done(_leftovers, entries) = wad_directory(buf, &header) {
-                return Some(WADArchive{ buffer: buf, header: header, directory: entries });
-            }
-            else {
-                return None;
-            }
+pub fn parse_wad(buf: &[u8]) -> IResult<&[u8], WADArchive> {
+    // FIXME ambiguous whether the error was from parsing the header or the entries
+    let (_, header) = try_parse!(buf, wad_header);
+
+    match wad_directory(buf, &header) {
+        IResult::Done(leftovers, entries) => {
+            let archive = WADArchive{ buffer: buf, header: header, directory: entries };
+            return IResult::Done(leftovers, archive);
         }
         IResult::Incomplete(needed) => {
-            println!("early termination what {:?}", needed);
-            return None;
+            return IResult::Incomplete(needed);
         }
         IResult::Error(err) => {
-            println!("boom!  {:?}", err);
-            return None;
+            return IResult::Error(err);
         }
     }
 }
 
 
+// -----------------------------------------------------------------------------
+// Map stuff
+
+// Standard lumps and whether they're required
+const MAP_LUMP_ORDER: [(&'static str, bool); 11] = [
+    ("THINGS",	 true),
+    ("LINEDEFS", true),
+    ("SIDEDEFS", true),
+    ("VERTEXES", true),
+    ("SEGS",	 false),
+    ("SSECTORS", false),
+    ("NODES",	 false),
+    ("SECTORS",	 true),
+    ("REJECT",	 false),
+    ("BLOCKMAP", false),
+    ("BEHAVIOR", false),
+];
+
+pub struct WADMapIterator<'a> {
+    archive: &'a WADArchive<'a>,
+    entry_iter: core::iter::Enumerate<std::slice::Iter<'a, WADDirectoryEntry<'a>>>,
+}
+
+impl<'a> Iterator for WADMapIterator<'a> {
+    type Item = WADMapEntryBlock;
+
+    fn next(&mut self) -> Option<WADMapEntryBlock> {
+        // Alas!  The compiler is not smart enough to realize that the
+        // following loop is inescapable without both of these getting values.
+        // TODO maybe i can reword to make this work?
+        //let mut marker_index = 0;
+        //let mut map_name = "";
+        let (marker_index, map_name);
+        loop {
+            if let Some((i, entry)) = self.entry_iter.next() {
+                if let IResult::Done(_leftovers, found_map_name) = vanilla_map_name(entry.name.as_bytes()) {
+                    marker_index = i;
+                    map_name = found_map_name;
+                    break;
+                }
+            }
+            else {
+                // Hit the end of the entries
+                return None;
+            }
+        }
+
+        let mut range = WADMapEntryBlock{
+            format: MapFormat::Doom,
+            name: map_name,
+            marker_index: marker_index,
+            last_index: marker_index,
+
+            things_index: None,
+            linedefs_index: None,
+            sidedefs_index: None,
+            vertexes_index: None,
+            segs_index: None,
+            ssectors_index: None,
+            nodes_index: None,
+            sectors_index: None,
+            reject_index: None,
+            blockmap_index: None,
+            behavior_index: None,
+            textmap_index: None,
+        };
+
+        let mut i;
+        let mut entry;
+        match self.entry_iter.next() {
+            Some((next_i, next_entry)) => {
+                i = next_i;
+                entry = next_entry;
+            }
+            None => { return None; }
+        }
+
+        if entry.name == "TEXTMAP" {
+            // This is a UDMF map, which has a completely different scheme: it
+            // goes until an explicit ENDMAP marker
+            range.format = MapFormat::UDMF;
+            // TODO continue this logic
+        }
+
+        for &(lump_name, is_required) in MAP_LUMP_ORDER.iter() {
+            // TODO i am pretty sure this is supposed to be case-insensitive?
+            if entry.name == lump_name {
+                match entry.name {
+                    "THINGS" => { range.things_index = Some(i); }
+                    "LINEDEFS" => { range.linedefs_index = Some(i); }
+                    "SIDEDEFS" => { range.sidedefs_index = Some(i); }
+                    "VERTEXES" => { range.vertexes_index = Some(i); }
+                    "SEGS" => { range.segs_index = Some(i); }
+                    "SSECTORS" => { range.ssectors_index = Some(i); }
+                    "NODES" => { range.nodes_index = Some(i); }
+                    "SECTORS" => { range.sectors_index = Some(i); }
+                    "REJECT" => { range.reject_index = Some(i); }
+                    "BLOCKMAP" => { range.blockmap_index = Some(i); }
+                    "BEHAVIOR" => { range.behavior_index = Some(i); }
+                    "TEXTMAP" => { range.textmap_index = Some(i); }
+                    _ => {
+                        // TODO wait, what's the right thing here
+                        break;
+                    }
+                }
+                match self.entry_iter.next() {
+                    Some((next_i, next_entry)) => {
+                        i = next_i;
+                        entry = next_entry;
+                    }
+                    None => {
+                        // FIXME this needs to check whether there are any
+                        // /required/ lumps not yet seen, ugh
+                        break;
+                    }
+                }
+            }
+            else if is_required {
+                // FIXME return a better error: expected lump X, found Y
+                // FIXME should this really stop us from iterating over any further maps?
+                // TODO should we try to cleverly detect what happened here?  what DID happen here, anyway?
+                // TODO maybe we should return what we have so far, and let the conversion to a real map take care of it?  but then how do we handle missing only one lump (do we grab the rest)?  what about duplicate lumps?
+                // TODO same questions go for the places i used try!(), except i think i got the logic even worse there, idk.  write some tests
+                return None;
+            }
+        }
+
+        range.last_index = i;
+        return Some(range);
+    }
+}
+// FIXME should this really be a list of general lump types?
+// FIXME not actually used lol
+/// List of lumps that can appear in a (non-UDMF) map, in this order.
+enum MapLumps {
+    /// A marker that provides the map name, usually ExMy or MAPxx.  Generally
+    /// empty, although FraggleScript is put in this lump.
+	Marker,             // A separator, name, ExMx or MAPxx
+
+    /// List of things (actors) in the map.  Format is different for Doom
+    /// versus Hexen maps.
+    Things,
+
+    /// List of lines in the map.  Format is different for Doom versus Hexen
+    /// maps.
+    Linedefs,
+
+    /// List of sidedefs in the map, the front and back sides of a line.
+    Sidedefs,
+
+    /// List of vertices in the map.
+    Vertexes,
+
+    Segs,
+    Subsectors,
+    Nodes,
+    Sectors,
+    Reject,
+    Blockmap,
+    Behavior,
+    Conversation,
+
+    // FIXME: also, ZNODES, GLZNODES, and of course TEXTMAP and ENDMAP
+}
+
+pub struct WADMapEntryBlock {
+    pub format: MapFormat,
+    pub name: MapName,  // TODO what are the rules in zdoom?  can you really use any map name?
+    pub marker_index: usize,
+    pub last_index: usize,
+
+    pub things_index: Option<usize>,
+    pub linedefs_index: Option<usize>,
+    pub sidedefs_index: Option<usize>,
+    pub vertexes_index: Option<usize>,
+    pub segs_index: Option<usize>,
+    pub ssectors_index: Option<usize>,
+    pub nodes_index: Option<usize>,
+    pub sectors_index: Option<usize>,
+    pub reject_index: Option<usize>,
+    pub blockmap_index: Option<usize>,
+    pub behavior_index: Option<usize>,
+    pub textmap_index: Option<usize>,
+    // TODO endmap
+}
+
+// TODO map parsing requires:
+// - come up with some way to treat a map as a single unit in a wad (is there anything else that acts this way?)
+// - parsers for:
+//   - THINGS
+//   - LINEDEFS
+//   - SIDEDEFS
+//   - SEGS
+//   - SSECTORS (deferrable)
+//   - NODES (deferrable)
+//   - SECTORS
+//   - REJECT (deferrable)
+//   - BLOCKMAP (deferrable)
+// - put all this in its own module/hierarchy
+
+// FIXME: vertices are i16 for vanilla, 15/16 fixed for ps/n64, effectively infinite but really f32 for udmf
+pub struct DoomVertex {
+    pub x: i16,
+    pub y: i16,
+}
+
+named!(vertexes_lump(&[u8]) -> Vec<DoomVertex>, terminated!(many0!(do_parse!(
+    x: le_i16 >>
+    y: le_i16 >>
+    (DoomVertex{ x: x, y: y })
+)), eof!()));
+
+
+// FIXME vertex/sidedef indices are i16 in vanilla, but extended to u16 in most source ports; note that for true vanilla, a negative index makes no sense anyway
+// FIXME hexen extends this, which requires detecting hexen format
+// FIXME what exactly is the higher-level structure that holds actual references to the sidedefs?
+pub struct DoomLine {
+    pub v0: i16,
+    pub v1: i16,
+    pub flags: i16,
+    pub special: i16,
+    pub sector_tag: i16,
+    // NOTE: -1 to mean none
+    pub front_sidedef: i16,
+    pub back_sidedef: i16,
+}
+
+named!(doom_linedefs_lump(&[u8]) -> Vec<DoomLine>, terminated!(many0!(do_parse!(
+    v0: le_i16 >>
+    v1: le_i16 >>
+    flags: le_i16 >>
+    special: le_i16 >>
+    sector_tag: le_i16 >>
+    front_sidedef: le_i16 >>
+    back_sidedef: le_i16 >>
+    (DoomLine{ v0: v0, v1: v1, flags: flags, special: special, sector_tag: sector_tag, front_sidedef: front_sidedef, back_sidedef: back_sidedef })
+)), eof!()));
+
+
+pub struct DoomSide {}
+pub struct DoomThing {}
+pub struct DoomSector {}
+
+/// The result of parsing a Doom-format map definition.  The contained
+/// structures have not been changed in any way.  Everything is public, and
+/// nothing is preventing you from meddling with the contained data in a way
+/// that might make it invalid.
+pub struct BareDoomMap {
+    pub vertices: Vec<DoomVertex>,
+    pub sectors: Vec<DoomSector>,
+    pub sides: Vec<DoomSide>,
+    pub lines: Vec<DoomLine>,
+    pub things: Vec<DoomThing>,
+}
+
+
+// TODO much more error handling wow lol
+pub fn parse_doom_map(archive: &WADArchive, range: &WADMapEntryBlock) -> Option<BareDoomMap> {
+    let vertices = if let Some(vertexes_index) = range.vertexes_index {
+        let buf = archive.entry_slice(vertexes_index);
+        if let IResult::Done(_leftovers, parsed_vertices) = vertexes_lump(buf) {
+            parsed_vertices
+        }
+        else {
+            return None;
+        }
+    }
+    else {
+        return None;
+    };
+
+    // TODO ugh how do i not repeat all this
+    let lines = if let Some(linedefs_index) = range.linedefs_index {
+        let buf = archive.entry_slice(linedefs_index);
+        if let IResult::Done(_leftovers, parsed_lines) = doom_linedefs_lump(buf) {
+            parsed_lines
+        }
+        else {
+            return None;
+        }
+    }
+    else {
+        return None;
+    };
+
+    return Some(BareDoomMap{
+        vertices: vertices,
+        lines: lines,
+        sectors: vec![],
+        sides: vec![],
+        things: vec![],
+    });
+}
 
 
 

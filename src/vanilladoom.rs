2480fa908002f006f0704a5d24d710ebc373f7dd
Federico Mena Quintero <federico@gnome.org>
2018-05-03 20:24:44 -0500

    Prefix unused statics with _

--- src/vanilladoom.rs
+++ src/vanilladoom.rs
@@ -3,7 +3,7 @@
 
 use super::universe::{ActorType, ThingCategory};
 
-static DOOM2_SPRITES: [&str; 138] = [
+static _DOOM2_SPRITES: [&str; 138] = [
     "TROO","SHTG","PUNG","PISG","PISF","SHTF","SHT2","CHGG","CHGF","MISG",
     "MISF","SAWG","PLSG","PLSF","BFGG","BFGF","BLUD","PUFF","BAL1","BAL2",
     "PLSS","PLSE","MISL","BFS1","BFE1","BFE2","TFOG","IFOG","PLAY","POSS",
